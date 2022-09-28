6e7429c6869e37c87b94383d5440dc53864d3836
ganesh <ganeshakkimardi@gmail.com>
2022-09-28 16:21:56 +0530

    new file vinod.txt

--- src/main/java/com/sony/UsersServiceApplication.java
+++ src/main/java/com/sony/UsersServiceApplication.java
@@ -3,6 +3,8 @@ package com.sony;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// this is a sample comment
+
 @SpringBootApplication
 public class UsersServiceApplication {
 
